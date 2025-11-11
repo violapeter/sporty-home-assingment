3bf3ba90138cddae78b07558c25ca3412717d437
violapeter <info@violapeter.hu>
2025-11-11 13:09:24 +0100

    modify the gateway to return full responses

--- features/Gateways/LeagueDataGateway.ts
+++ features/Gateways/LeagueDataGateway.ts
@@ -1,30 +1,15 @@
 import { LeaguesApiClient } from 'api-client'
-import type { League, SeasonBadge, SportType } from 'shared-types'
+import type { BadgesResponse, LeaguesResponse } from 'api-client'
 
 export class LeagueDataGateway {
   constructor(private apiClient: LeaguesApiClient) {}
 
-  async getLeagues(): Promise<League[]> {
-    const response = await this.apiClient.leagues.list()
-
-    return response.leagues.map(
-      ({ strLeague, strLeagueAlternate, idLeague, strSport }) => ({
-        id: idLeague,
-        name: strLeague,
-        alternateName: strLeagueAlternate,
-        sport: strSport as SportType,
-      }),
-    )
+  async getLeagues(): Promise<LeaguesResponse> {
+    return this.apiClient.leagues.list()
   }
 
-  async getSeasonBadge(id: string): Promise<SeasonBadge> {
-    const response = await this.apiClient.badges.list({ id })
-    const { strSeason, strBadge } = response.badges[0]
-
-    return {
-      season: strSeason,
-      badge: strBadge,
-    }
+  async getSeasonBadges(id: string): Promise<BadgesResponse> {
+    return this.apiClient.badges.list({ id })
   }
 }
 
