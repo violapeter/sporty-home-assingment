ad6c9eaa4370432b16d69f7d6b1fc1a453c10fb3
violapeter <info@violapeter.hu>
2025-11-11 21:56:55 +0100

    add layout styling

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,13 @@
 import { LeagueList } from './components/LeagueList.tsx'
 
 export const App = () => (
-  <>
-    <LeagueList />
-  </>
+  <div className="Layout">
+    <header className="Layout__Header">Peter Viola - Home Assignment</header>
+    <main className="Layout__Content">
+      <LeagueList />
+    </main>
+    <footer className="Layout__Footer">
+      &copy; {new Date().getFullYear()} Peter Viola
+    </footer>
+  </div>
 )
