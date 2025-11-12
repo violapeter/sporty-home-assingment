fb4d23a5f579b5b44078b6110c5000a16bf3668e
violapeter <info@violapeter.hu>
2025-11-12 10:15:46 +0100

    add links to the header

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,7 +2,25 @@ import { LeagueList } from './components/LeagueList.tsx'
 
 export const App = () => (
   <div className="Layout">
-    <header className="Layout__Header">Peter Viola - Home Assignment</header>
+    <header className="Layout__Header Header">
+      <a target="_blank" className="Header__Link" href="https://violapeter.hu/">
+        Peter Viola
+      </a>
+      <a
+        target="_blank"
+        className="Header__Link"
+        href="https://github.com/violapeter/"
+      >
+        GitHub
+      </a>
+      <a
+        target="_blank"
+        className="Header__Link"
+        href="https://linkedin.com/in/violapeter/"
+      >
+        LinkedIn
+      </a>
+    </header>
     <main className="Layout__Content">
       <LeagueList />
     </main>
--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -4,7 +4,23 @@ import LeagueList from './components/LeagueList.vue'
 
 <template>
   <div class="Layout">
-    <header class="Layout__Header">Peter Viola - Home Assignment</header>
+    <header class="Layout__Header Header">
+      <a target="_blank" class="Header__Link" href="https://violapeter.hu/"
+        >Peter Viola</a
+      >
+      <a
+        target="_blank"
+        class="Header__Link"
+        href="https://github.com/violapeter/"
+        >GitHub</a
+      >
+      <a
+        target="_blank"
+        class="Header__Link"
+        href="https://linkedin.com/in/violapeter/"
+        >LinkedIn</a
+      >
+    </header>
     <main class="Layout__Content">
       <LeagueList />
     </main>
